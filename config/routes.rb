3f4bfeb5a82aa005080c0115068bb0ec6a3b96ec
Sistemas <rails@railsbox.(none)>
2013-06-20 16:48:21 -0300

    nuevo home

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ ProyectoFinal::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
