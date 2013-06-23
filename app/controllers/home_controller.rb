eb058fb8a2e6b3f79a2dac7e209c91e94ea0c9e7
Sistemas <rails@railsbox.(none)>
2013-06-23 17:36:52 -0300

    index con publicaciones

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    @publications  = Publication.all
+    @users = User.all
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
