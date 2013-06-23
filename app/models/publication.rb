e5cb9e3a54b489ce0993219ace9967c8549e839c
Sistemas <rails@railsbox.(none)>
2013-06-23 18:18:04 -0300

    index editado ;)

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,4 +1,4 @@
 class Publication < ActiveRecord::Base
-  attr_accessible :description, :image, :title, :user_id
-  has_attached_file :image, :styles => { :medium => "480x480>", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
+  attr_accessible :description, :image, :title, :user_id, :created_at
+  has_attached_file :image, :styles => { :medium => "680x680>", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
 end
