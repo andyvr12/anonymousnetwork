d8b084eb132c8df7bcb160cc1cb6156341b3d61e
Sistemas <rails@railsbox.(none)>
2013-06-24 18:27:54 -0300

    correción clase

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,14 @@
 class HomeController < ApplicationController
 
   def index
-    @publications  = Publication.all
+    @publications  = Publication.find(:all, :order => 'created_at DESC')
     @users = User.all
     @comments = Comment.all
+    @comment = Comment.new
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
     end
+
   end
 end
